b4b1f69c1cfba09b06933da15e7192f8449dcf2d
Igor Isaias Banlian <igorisaiasbanlian@gmail.com>
2025-06-20 01:47:22 -0300

    Update transmac-trial-script.py

--- transmac-trial-script.py
+++ transmac-trial-script.py
@@ -1,32 +1,46 @@
 import winreg
+import re
 
 def subkeys(key):
     i = 0
     while True:
         try:
-            subkey = winreg.EnumKey(key, i)
-            yield subkey
-            i+=1
-        except WindowsError as e:
+            yield winreg.EnumKey(key, i)
+            i += 1
+        except OSError:
             break
 
+def is_guid(text):
+    return re.fullmatch(r'\{[0-9A-Fa-f\-]{36}\}', text) is not None
+
 def traverse_registry_tree(hkey, keypath, tabs=0):
-    notdone = True
-    key = winreg.OpenKey(hkey, keypath, 0, winreg.KEY_READ)
-    for subkeyname in subkeys(key):
-        foundkey = '\t'*tabs + subkeyname
-        subkeypath = "%s\\%s" % (keypath, subkeyname)
-        traverse_registry_tree(hkey, subkeypath, tabs+1)
-        if len(foundkey) == 38:
-            fullp = keypath + '\\\\' + foundkey
-            open_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, fullp, 0, winreg.KEY_ALL_ACCESS)
-            winreg.DeleteKey(open_key, '')
-            print("Deleted: " + fullp)
-            print("")
-            print("Trial has been reset!")
-            notdone = False
-    if notdone:
+    changed = False
+    try:
+        key = winreg.OpenKey(hkey, keypath, 0, winreg.KEY_READ)
+    except FileNotFoundError:
+        return False
+
+    for subkeyname in list(subkeys(key)):
+        subkeypath = f"{keypath}\\{subkeyname}"
+        
+        if traverse_registry_tree(hkey, subkeypath, tabs+1):
+            changed = True
+
+        if is_guid(subkeyname):
+            try:
+                winreg.DeleteKey(hkey, subkeypath)
+                print(f"Deleted: {subkeypath}")
+                print("Trial has been reset!\n")
+                changed = True
+            except PermissionError:
+                print(f"Permission denied: {subkeypath}")
+            except FileNotFoundError:
+                pass
+
+    if tabs == 0 and not changed:
         print("No key to delete found! No changes were made to your registry.")
 
-keypath = r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Shell Extensions\\Approved"
+    return changed
+
+keypath = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved"
 traverse_registry_tree(winreg.HKEY_CURRENT_USER, keypath)
